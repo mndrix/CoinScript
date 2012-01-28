fd68a579280b6c8716ed09ea10e145416d6cf5c5
Michael Hendricks <michael@ndrix.org>
2012-01-27 21:33:35 -0700

    First working, tiny stack language

---
+++ src/Language/CoinScript.hs
@@ -0,0 +1,38 @@
+module Language.CoinScript
+    (
+    ) where
+
+import Data.Char
+
+data Op = OpNoop
+        | OpInt Integer
+        | OpAdd
+    deriving (Show)
+
+type Program = [Op]
+
+data Item = ItemInt Integer
+    deriving (Show)
+type Stack = [Item]
+
+-- Parse script text into an executable program
+parse :: String -> Program
+parse str = reverse $ go str []
+  where
+    go []       p = p
+    go (' ':cs) p = go cs (OpNoop:p)
+    go ('+':cs) p = go cs (OpAdd:p)
+    go s@(c:_)  p
+        | isDigit c =
+            let (digits,rest) = span isDigit s in
+            go rest ((OpInt $ read digits) : p)
+    go (_:cs) p = go cs p
+
+runOp :: Op -> Stack -> Stack
+runOp OpNoop s = s
+runOp (OpInt i) s = ItemInt i : s
+runOp OpAdd (ItemInt x : ItemInt y : s) = ItemInt (x+y) : s
+runOp OpAdd _ = error "+ requires two integers on the stack"
+
+run :: Program -> Stack -> Stack
+run p s = foldl (flip runOp) s p
