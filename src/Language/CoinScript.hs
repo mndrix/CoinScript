5b6f0432540a39637cdb3e75e355ec8460f87c85
Michael Hendricks <michael@ndrix.org>
2012-01-27 21:42:21 -0700

    Add dup and drop operations

--- src/Language/CoinScript.hs
+++ src/Language/CoinScript.hs
@@ -7,6 +7,8 @@ import Data.Char
 data Op = OpNoop
         | OpInt Integer
         | OpAdd
+        | OpDup
+        | OpDrop
     deriving (Show)
 
 type Program = [Op]
@@ -22,6 +24,8 @@ parse str = reverse $ go str []
     go []       p = p
     go (' ':cs) p = go cs (OpNoop:p)
     go ('+':cs) p = go cs (OpAdd:p)
+    go ('d':cs) p = go cs (OpDup:p)
+    go ('D':cs) p = go cs (OpDrop:p)
     go s@(c:_)  p
         | isDigit c =
             let (digits,rest) = span isDigit s in
@@ -33,6 +37,10 @@ runOp OpNoop s = s
 runOp (OpInt i) s = ItemInt i : s
 runOp OpAdd (ItemInt x : ItemInt y : s) = ItemInt (x+y) : s
 runOp OpAdd _ = error "+ requires two integers on the stack"
+runOp OpDup s@(x:_) = x:s
+runOp OpDup _ = error "d requires something on the stack"
+runOp OpDrop (x:s) = s
+runOp OpDrop _ = error "D requires something on the stack"
 
 run :: Program -> Stack -> Stack
 run p s = foldl (flip runOp) s p
