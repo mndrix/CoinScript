cdc2fc07a0d16a0ca1677bf9098f012682d92e19
Michael Hendricks <michael@ndrix.org>
2012-01-28 20:26:36 -0700

    Add a stack machine that holds types
    This implements type inference.  By running a script against
    DataMachine, you get specific results.  By running it against a
    TypeMachine, you get the program's output type signature.
    
    Seeing whether this would work at all was my goal when starting out
    last night.  It's alive!

--- src/Language/CoinScript.hs
+++ src/Language/CoinScript.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
 module Language.CoinScript
     (
     ) where
@@ -14,14 +16,20 @@ data Op = OpNoop
 
 type Program = [Op]
 
+-- contents of a data stack
 data Item = ItemInt Integer
     deriving (Show)
 
+-- contents of a type stack
+data Type = TypeInt
+          | TypeUnknown
+    deriving (Show)
+
 -- describes functions applicable to all stack machines
-class Machine a where
-    push :: Item -> State a ()
-    pop :: State a Item
-    peek :: State a Item
+class Machine a b | a -> b where
+    push :: b -> State a ()
+    pop :: State a b
+    peek :: State a b
     peek = do
         x <- pop
         push x
@@ -31,9 +39,9 @@ class Machine a where
 
 -- a stack machine for operating on data
 data DataMachine = DataMachine
-    { stack :: [Item]
+    { itemStack :: [Item]
     } deriving (Show)
-instance Machine DataMachine where
+instance Machine DataMachine Item where
     push i = do
         (DataMachine s) <- get
         put $ DataMachine (i:s)
@@ -45,6 +53,23 @@ instance Machine DataMachine where
     popInteger = do
         (ItemInt i) <- pop
         return i
+
+-- a stack machine for operating on types
+data TypeMachine = TypeMachine
+    { typeStack :: [Type]
+    } deriving (Show)
+instance Machine TypeMachine Type where
+    push t = do
+        (TypeMachine s) <- get
+        put $ TypeMachine (t:s)
+    pop = do
+        (TypeMachine (t:s)) <- get
+        put $ TypeMachine s
+        return t
+    pushInteger _ = push TypeInt
+    popInteger = do
+        TypeInt <- pop
+        return 1
 
 -- Parse script text into an executable program
 parse :: String -> Program
@@ -61,7 +86,7 @@ parse str = reverse $ go str []
             go rest ((OpInt $ read digits) : p)
     go (_:cs) p = go cs p
 
-runOp :: Machine a => Op -> State a ()
+runOp :: Machine a b => Op -> State a ()
 runOp OpNoop = return ()
 runOp (OpInt i) = pushInteger i
 runOp OpAdd = do
@@ -71,5 +96,5 @@ runOp OpAdd = do
 runOp OpDup = peek >>= push
 runOp OpDrop = pop >> return ()
 
-run :: Machine a => Program -> a -> a
+run :: Machine a b => Program -> a -> a
 run p st = foldl (\s o -> execState (runOp o) s) st p
