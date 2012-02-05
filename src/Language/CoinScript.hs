855686d3159ff06865138152bab52ad595181f55
Michael Hendricks <michael@ndrix.org>
2012-02-04 21:35:06 -0700

    Some notes about other machines

--- src/Language/CoinScript.hs
+++ src/Language/CoinScript.hs
@@ -53,6 +53,27 @@ data Type = TypeInt
           | TypeVar Int
     deriving (Eq,Show)
 
+{- TODO ideas for other interesting machines
+ -
+ - * dynamically typed machine (converts to/from types as needed)
+ - * string evaluation machine (runs via pattern matching, like Joy can)
+ - * LLVM machine (generates native code)
+ -
+ - I'm starting to think that more of the computation should move into
+ - the machine itself.  One should be able to creat an empty machine,
+ - load it with code, step it forward and ask if it's done.  All this
+ - is done in the StateE monad so errors are handled nicely.  Everything
+ - else is an implementation detail.
+ -
+ - Unfortunately, this design means that each machine has to implement
+ - the operations for itself.  If there's a way to leave 'runOp' generic
+ - like it is now, that'd be fantastic.  It might work for everything
+ - besides the string evaluation machine.  That machine doesn't even
+ - have a stack.  It just has an instruction tape (the string) that's
+ - modified by repeated pattern matching.  Similarly, the LLVM machine
+ - probably wouldn't have a stack.
+ -}
+
 -- describes functions applicable to all stack machines
 class Machine a b | a -> b where
     empty :: a
